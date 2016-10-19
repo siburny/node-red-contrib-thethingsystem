51559d2567a8bf8d80f36566cbd91dbc86764b16
Maxim Rubis <siburny@gmail.com>
2016-10-18 23:11:06 -0400

    First version

---
+++ test.js
@@ -0,0 +1,25 @@
+var ClientAPI = require('taas-client');
+
+var steward = new ClientAPI.ClientAPI({
+	steward: { name: '127.0.0.1' } 
+})
+.on('open', function(channel, loginP) {
+  console.log('console');
+}).on('ready', function(channel, data) {
+  // when the management channel is ready, it's time to get to work!
+  if (channel !== 'management') return;
+  console.log('ready');
+  
+  this.listDevice('', {}, function(done) {
+	  console.log(done);
+    steward.performDevice(5, "off", {}, function() {
+    steward.performDevice(5, "on", {}, function() {
+    });
+    });
+  });
+  
+}).on('close', function(channel) {
+	console.log('close');
+}).on('error', function(err, channel) {
+	console.log('error');
+});
