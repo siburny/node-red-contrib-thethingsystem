22a1b3663585850486cb5f221e03813f6306ab86
Maxim Rubis <siburny@gmail.com>
2016-10-30 22:06:42 -0400

    0.02 version (first functional version)
    Add 'when' promise library
    Finish configuration node
    Add selector for a device

--- thethingsystem/steward-server.js
+++ thethingsystem/steward-server.js
@@ -1,7 +1,45 @@
-module.exports = function(RED) {
-    function StewardServerNode(n) {
-        RED.nodes.createNode(this, n);
-        this.host = n.host;
-    }
-    RED.nodes.registerType('steward-server', StewardServerNode);
+module.exports = function (RED) {
+  var ClientAPI = require('taas-client');
+  var when = require('when');
+
+  function StewardServerNode(n) {
+    RED.nodes.createNode(this, n);
+
+    var node = this;
+
+    node.host = n.host;
+    node.connected = false;
+    node.steward = new ClientAPI.ClientAPI({
+      steward: { name: node.host },
+      logger: {
+        error: function (msg, props) { }
+        , warning: function (msg, props) { }
+        , notice: function (msg, props) { }
+        , info: function (msg, props) { }
+        , debug: function (msg, props) { }
+      }
+    }).on('ready', function (channel, data) {
+      if (channel !== 'management') return;
+      node.connected = true;
+    }).on('close', function (channel) {
+      console.log('close');
+    }).on('error', function (err, channel) {
+      console.log('error');
+    });
+
+    RED.httpAdmin.get('/steward/devices', function (req, res) {
+      if (!node.connected)
+        return [];
+
+      node.steward.listDevice('', { depth: 'all' }, function (data) {
+        if (!!data && !!data.result && !!data.result.devices) {
+          res.json(data.result.devices);
+        } else {
+          res.json([]);
+        }
+      });
+    });
+  }
+
+  RED.nodes.registerType('steward-server', StewardServerNode);
 }
--- thethingsystem/thing.js
+++ thethingsystem/thing.js
@@ -1,8 +1,23 @@
-module.exports = function(RED) {
-    function StewardThingNode(n) {
-        RED.nodes.createNode(this, n);
-        console.log(n);
-        this.host = n.host;
-    }
-    RED.nodes.registerType('thing', StewardThingNode);
+module.exports = function (RED) {
+  function StewardThingNode(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+    node.server = RED.nodes.getNode(config.server)
+    node.thing = config.thing;
+    node.name = config.name;
+
+    this.on('input', function(msg) {
+      if(!!node.server && !!node.server.connected) {
+        console.log(node);
+        node.server.steward.performDevice(node.thing, msg.payload, {}, function() { });
+        if(msg.payload === 'on') {
+          node.status({fill: 'green', shape: 'dot', text: 'ON'});
+        } else if(msg.payload === 'off') {
+          node.status({fill: 'red', shape: 'dot', text: 'OFF'});
+        }
+      }
+    });
+  }
+  RED.nodes.registerType('thing', StewardThingNode);
 }
