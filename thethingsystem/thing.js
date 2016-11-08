38108534574f25773bd56f1b4f4e79334575cdee
Maxim Rubis <siburny@gmail.com>
2016-11-07 20:55:56 -0500

    Add Perform suffix to node name

--- thethingsystem/thing.js
+++ thethingsystem/thing.js
@@ -1,5 +1,5 @@
 module.exports = function (RED) {
-  function StewardThingNode(config) {
+  function StewardThingNodePerform(config) {
     RED.nodes.createNode(this, config);
 
     var node = this;
@@ -19,5 +19,27 @@ module.exports = function (RED) {
       }
     });
   }
-  RED.nodes.registerType('thing', StewardThingNode);
+  RED.nodes.registerType('thing', StewardThingNodePerform);
+
+  function StewardThingNodeObserve(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+    node.server = RED.nodes.getNode(config.server)
+    node.thing = config.thing;
+    node.name = config.name;
+
+    this.on('input', function(msg) {
+      if(!!node.server && !!node.server.connected) {
+        console.log(node);
+        node.server.steward.performDevice(node.thing, msg.payload, {}, function() { });
+        if(msg.payload === 'on') {
+          node.status({fill: 'green', shape: 'dot', text: 'ON'});
+        } else if(msg.payload === 'off') {
+          node.status({fill: 'red', shape: 'dot', text: 'OFF'});
+        }
+      }
+    });
+  }
+  RED.nodes.registerType('thing', StewardThingNodePerform);
 }
