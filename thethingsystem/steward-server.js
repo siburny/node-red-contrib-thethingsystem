73f3e3bb51ecba24e12bd5ab721235d78fc60a5a
Maxim Rubis <siburny@gmail.com>
2017-10-28 13:26:09 -0400

    Move device refresh to backend

--- thethingsystem/steward-server.js
+++ thethingsystem/steward-server.js
@@ -1,6 +1,7 @@
 module.exports = function (RED) {
   var ClientAPI = require('taas-client');
   var when = require('when');
+  var steward;
 
   function StewardServerNode(n) {
     RED.nodes.createNode(this, n);
@@ -10,7 +11,7 @@ module.exports = function (RED) {
     node.host = n.host;
     node.connected = false;
 
-    node.steward = new ClientAPI.ClientAPI({
+    steward = node.steward = new ClientAPI.ClientAPI({
       steward: { name: node.host },
       logger: {
         error: function (msg, props) { }
@@ -26,7 +27,7 @@ module.exports = function (RED) {
     }).on('error', function (err, channel) {
     });
 
-    node.on('close', function() {
+    node.on('close', function () {
       node.connected = false;
       node.steward.close();
       delete node.steward;
@@ -34,4 +35,18 @@ module.exports = function (RED) {
   }
 
   RED.nodes.registerType('steward-server', StewardServerNode);
+
+  RED.httpAdmin.get("/stewardListDevices", RED.auth.needsPermission("steward-server.write"), function (req, res) {
+    if (steward && steward.readyP) {
+      steward.listDevice('', {}, function(data) {
+        if(data) {
+          res.json(data);
+        } else {
+          res.sendStatus(404);
+        }
+      });
+    } else {
+      res.sendStatus(403);
+    }
+  });
 }
